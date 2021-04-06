7a0c4bea6fce7baab157e6e91fe8590cd779844e
Fraza Aditya Wiguna <67851409+FrazaAdityaWiguna@users.noreply.github.com>
2021-04-06 11:03:12 +0700

    Setup Trunk

--- src/config/redux/action/index.js
+++ src/config/redux/action/index.js
@@ -1,5 +1,23 @@
-export const actionUser = () => (dispatch) => {
-  setTimeout(() => {
-    return dispatch({ type: "CHANGE_USER", value: "Fraza" });
-  }, 2000);
+import firebase from "../../firebase";
+
+export const registerUserAPI = (data) => (dispatch) => {
+  dispatch({ type: "CHANGE_LOADING", value: true });
+  return firebase
+    .auth()
+    .createUserWithEmailAndPassword(data.email, data.password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+      console.log("user", user);
+      dispatch({ type: "CHANGE_LOADING", value: false });
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+      console.log(errorCode);
+      console.log(errorMessage);
+      dispatch({ type: "CHANGE_LOADING", value: false });
+    });
 };
